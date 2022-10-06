79f6ad33547b1b95172123c30f1c70b6e0983020
Vladimir Vashchenko <dn090884vvm@gmail.com>
2022-10-06 11:50:35 +0300

    adding checkings in thee patch controller

--- controllers/contacts/updateFavoriteStatus.js
+++ controllers/contacts/updateFavoriteStatus.js
@@ -5,6 +5,14 @@ const createError = require("http-errors");
 const updateFavoriteStatus = async (req, res) => {
   const { contactId } = req.params;
   const { favorite } = req.body;
+
+  if (!favorite) {
+    res.status(400).json({
+      status: 400,
+      message: "missing field favorite",
+    });
+  }
+
   const result = await Contact.findByIdAndUpdate(
     contactId,
     { favorite },
@@ -12,9 +20,15 @@ const updateFavoriteStatus = async (req, res) => {
       new: true,
     }
   );
+
   if (!result) {
+    res.status(404).json({
+      status: 404,
+      message: "Not found",
+    });
     throw createError(404, `Contact with id ${contactId} not found`);
   }
+
   res.json({
     status: "success",
     code: 200,
