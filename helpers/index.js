fd794b8abed76df121a7347dae12aea6283e680f
Vladimir Vashchenko <dn090884vvm@gmail.com>
2022-10-16 11:11:15 +0300

    finishing
    adding unique to database
    adding helper

--- helpers/index.js
+++ helpers/index.js
@@ -1,3 +1,4 @@
 const RequestError = require("./RequestError");
+const handleSchemaValidationsErrors = require("./handleSchemaValidationErrors");
 
-module.exports = { RequestError };
+module.exports = { RequestError, handleSchemaValidationsErrors };
