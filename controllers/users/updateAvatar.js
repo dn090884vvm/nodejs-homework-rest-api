932ea9e53b7020d88b41f858eebc0c937f547d61
Vladimir Vashchenko <dn090884vvm@gmail.com>
2022-10-18 16:32:35 +0300

    finishing task
    tests are not working correctly

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,6 +1,7 @@
 const { User } = require("../../models");
 const path = require("path");
 const fs = require("fs").promises;
+const jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
@@ -11,6 +12,8 @@ const updateAvatar = async (req, res) => {
   try {
     const resultUpload = path.join(avatarsDir, imageName);
     await fs.rename(tempUpload, resultUpload);
+    const file = await jimp.read(resultUpload);
+    await file.resize(250, 250).write(resultUpload);
     const avatarURL = path.join("public", "avatars", imageName);
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
 
